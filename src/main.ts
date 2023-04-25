4d90bc83852655bd7e370fac8b279bf8bd8cd919
zjy <jinyou.zeng@wukongsch.com>
2023-04-25 10:34:38 +0800

    添加百度统计

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,25 @@ import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 import router from './router'
 
+// 百度统计代码
 
+// @ts-ignore
+const _hmt: any[] = _hmt || [];
+
+  (function() {
+    const hm = document.createElement('script');
+    hm.src = 'https://hm.baidu.com/hm.js?5cb4fa061ea3314a61a3e68acdefd747';
+    const s = document.getElementsByTagName('script')[0];
+    // @ts-ignore
+    s.parentNode.insertBefore(hm, s);
+  })();
+router.beforeEach((to, from, next) => {
+  if (_hmt) {
+    if (to.path) {
+      _hmt.push(['_trackPageview', '/#' + to.fullPath])
+    }
+  }
+  next()
+})
 
 createApp(App).use(router).use(Antd).mount('#app')
